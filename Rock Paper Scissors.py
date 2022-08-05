f3d76b60c2c4e265654ba136a0dc05fe2bdb1bb4
CHAITANYA2605 <63853464+CHAITANYA2605@users.noreply.github.com>
2022-08-05 22:00:57 +0530

    source code FILE

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,20 @@
+import random
+def rpc():
+    
+    player=input("Pick any one- 'r' for rock,'p' for papaer,'s' for scissors")
+    pc=random.choice(['r','p','s'])
+
+    if player==pc:
+        return 'it\'s a draw'
+    
+    if won(player,pc):
+        return 'you won'
+
+    return'you lost'
+
+def won(player1,pc1):
+    if(player1=='r'and pc1=='s') or (player1=='s' and pc1=='p') or (player1=='p' and pc1 == 'r'):
+        return True
+        
+    print (rpc())
+    print("computer choice was ",pc)
